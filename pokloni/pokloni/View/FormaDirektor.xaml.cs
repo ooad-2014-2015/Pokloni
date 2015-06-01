b67a12954560df70021477c34d61aca6fabba52f
ajdinm <ajdinm.m@]gmail.com>
2015-06-01 22:28:40 +0200

    Pokusaj kamere

--- pokloni/pokloni/View/FormaDirektor.xaml.cs
+++ pokloni/pokloni/View/FormaDirektor.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using pokloni.ViewModel1;
 using MultivariableMetricVisualisation;
+using System.IO;
+using WebCam_Capture;
+using pokloni.View;
+using AForge.Video.DirectShow;
+using WPFMediaKit.DirectShow.Controls;
 
 namespace pokloni
 {
@@ -20,9 +25,17 @@ namespace pokloni
     /// Interaction logic for Window1.xaml
     /// </summary>
     public partial class Window1 : Window {
+        Webcam webcam;
+        VideoCaptureDevice v;
+        VideoCaptureElement vv;
+
         public Window1() {
             InitializeComponent();
-            DataContext = new FormaDirektorViewModel();  
+            DataContext = new FormaDirektorViewModel();
+            imgVideo = new Image();
+            webcam = new Webcam();
+            webcam.InitializeWebCam(ref imgVideo);
+            webcam.Start();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
@@ -55,6 +68,9 @@ namespace pokloni
 
         private void StackPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
 
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
         }
 
     }
